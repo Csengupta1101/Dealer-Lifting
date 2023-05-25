10ee9045fb7b21b8c3dc9d714145b76deaa521bc
Chandan Sengupta <chandan.sengupta@shyamsteel.net>
2023-05-25 13:09:18 +0530

    lifting model commit

--- app.py
+++ app.py
@@ -15,11 +15,11 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    data=[float(x) for x in request.form.values()]
+    data=[int(x) for x in request.form.values()]
     final_input=scalar.transform(np.array(data).reshape(1,-1))
-    print(final_input)
-    output=regmodel.predict(final_input)[0]
-    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
+    output=regmodel.predict(final_input)
+    output = round(output[0],2)
+    return jsonify(output)
 
 if __name__ == "__main__":
     app.run(debug=True)
